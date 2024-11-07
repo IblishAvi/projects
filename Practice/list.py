3678831defc04041965fc744e9f56afd949fa0da
iblishavi <avinashkumarjha842000@gmail.com>
2024-11-07 22:46:31 +0530

    Light Weight programs

--- Practice/list.py
+++ Practice/list.py
@@ -2,12 +2,35 @@
 
 names = ["Avi", "Naman", "Utkarsh", "ashutosh kumar"] #List of String
 print(names)
-names.append("Ananya") #Append module to add Ananya to list
-print(names)
-names.insert(1,"Rishu") #Insert function to insert at the specific end.
-print(names)
-friends = ["Avi", "Rohan", "Anmol", "Sukrit", "Ashutosh"]
-friends.extend(["beauty","Arti"])
-print(friends)
-names.pop(1)
+# names.append("Ananya") #Append module to add Ananya to list
+# print(names)
+# names.insert(1,"Rishu") #Insert function to insert at the specific end.
+# print(names)
+# friends = ["Avi", "Rohan", "Anmol", "Sukrit", "Ashutosh"]
+# friends.extend(["beauty","Arti"])
+# print(friends)
+# names.pop(1)
+num = [24,12,22,25,27,32]
+num.reverse()
+print(num)
+num.sort()
+print(num)
+print(num)
+print(num[1:])
+print(num[1:4])
+
+#Array in Python
+arr = [12,"Avi","lri"]
+print(arr)
+arr.pop()
+print(arr)
+arr.pop()
+print(arr)
+arr.insert(1,99)
+print(arr)
+
+arr.extend([85,54,46,76,98])
+print(arr)
+num =arr.copy()
+print(num)
 
