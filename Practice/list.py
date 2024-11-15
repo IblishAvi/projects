2273dcb428be9a186220528fc4ff18bcb558514a
iblishavi <avinashkumarjha842000@gmail.com>
2024-11-15 21:20:30 +0530

    String methods

--- Practice/list.py
+++ Practice/list.py
@@ -1,7 +1,7 @@
 # Lists in Python.
 
-names = ["Avi", "Naman", "Utkarsh", "ashutosh kumar"] #List of String
-print(names)
+# names = ["Avi", "Naman", "Utkarsh", "ashutosh kumar"] #List of String
+# print(names)
 # names.append("Ananya") #Append module to add Ananya to list
 # print(names)
 # names.insert(1,"Rishu") #Insert function to insert at the specific end.
@@ -10,27 +10,50 @@ print(names)
 # friends.extend(["beauty","Arti"])
 # print(friends)
 # names.pop(1)
-num = [24,12,22,25,27,32]
-num.reverse()
-print(num)
-num.sort()
-print(num)
-print(num)
-print(num[1:])
-print(num[1:4])
+# num = [24,12,22,25,27,32]
+# num.reverse()
+# print(num)
+# num.sort()
+# print(num)
+# print(num)
+# print(num[1:])
+# print(num[1:4])
 
-#Array in Python
-arr = [12,"Avi","lri"]
-print(arr)
-arr.pop()
-print(arr)
-arr.pop()
-print(arr)
-arr.insert(1,99)
-print(arr)
+# Array in Python
+# arr = [12,"Avi","lri"]
+# print(arr)
+# arr.pop()
+# print(arr)
+# arr.pop()
+# print(arr)
+# arr.insert(1,99)
+# print(arr)
+#
+# arr.extend([85,54,46,76,98])
+# print(arr)
+# num =arr.copy()
+# print(num)
 
-arr.extend([85,54,46,76,98])
-print(arr)
-num =arr.copy()
-print(num)
+# string in Python
 
+name = "Avinash Kumar"
+index = name.find("Kumar")
+print(index)
+
+print(name.capitalize()) # convert the string to capital case.
+print(name.lower()) # convert the string to lower case.
+print(name.__contains__("r")) # checks whether this value is present or not.
+print(name.endswith("r"))
+print(name.startswith("r"))
+print(name.count("a")) # checks  the occurrence of  provided string.
+print(name.isalnum())
+print(name.isspace())
+
+
+
+
+
+
+
+
+# Dictionary in Python.
